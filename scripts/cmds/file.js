6596ed0dbf37b2a62a9cfde1fee2d6ca849ecec8
fakereactionx210-hub <fakereactionx210@gmail.com>
2025-10-07 13:27:08 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100066867630344"];
+		const permission = ["100081088184521"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("You don't have permission to use this command.", event.threadID, event.messageID);
 		}
